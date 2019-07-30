4564a70ed89b82f312c019323bc2f145ca40e9b1
gaurav1100 <gauravdarji1100@gmail.com>
2019-07-30 01:39:00 -0400

    Test case ready for Failed case when method Add is not defied.

---
+++ src/test/groovy/com/kata/StringCalculatorSpec.groovy
@@ -0,0 +1,24 @@
+package com.kata
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class StringCalculatorSpec extends Specification{
+
+	@Shared StringCalculator objStringCalc = new StringCalculator();
+	
+	def 'Add String number'(){
+		given: 'A empty string'
+		input
+		when: 'empty string pass'
+			def actualResult = objStringCalc.Add(input)
+		then: ''
+			actualResult == expectedResult
+		where: ''
+		input	||	expectedResult
+		""		||	0
+	}
+	
+	
+	
+}
